0c858e77375425fb3142d74a17b356242390648b
specialmindsaarhus <mask@specialminds.dk>
2024-10-12 09:27:46 +0200

    animations fixed to fire on image finish loading -working

--- src/scripts/fetch.js
+++ src/scripts/fetch.js
@@ -93,12 +93,27 @@ function fetchCat() {
             //append the text inside fetchContainer
             //document.getElementById("fetchContainer").prependChild(textElement);
         })
-        .then(() => {
-            animate(
-                "#fetchContainer", { y: [400, 0] }, { duration: 0.5, easing: "ease-out" }
-            );
-        })
         .catch((error) => console.log("error", error));
+}
+
+function animateOut() {
+    document.getElementById("fetchImage").style.opacity = 0;
+    animate(
+        "#fetchContainer", { y: [0, "-100vh"] }, { duration: 0.5, easing: "ease-out" }
+    );
+}
+
+function loadImageIn() {
+    //document.getElementById("fetchImage").style.opacity = 0;
+    animate(
+        "#fetchImage", { opacity: [0, 1] }, { duration: 0.2, easing: "ease-out" }
+    );
+}
+
+function loadIn() {
+    animate(
+        "#fetchContainer", { y: [400, 0], opacity: [0, 1] }, { duration: 0.5, easing: "ease-out" }
+    );
 }
 
 // Run the fetch operation when the DOM is fully loaded
@@ -108,4 +123,20 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 document.getElementById("newCat").addEventListener("click", (event) => {
     fetchCat();
+    animateOut();
 });
+
+//tjek the image is loaded
+const imageElement = document.getElementById("fetchImage");
+
+imageElement.addEventListener("load", () => {
+    console.log("Image has loaded!");
+    loadIn();
+    loadImageIn();
+});
+
+// Check if the image is already loaded
+if (imageElement.complete) {
+    console.log("Image is already loaded.");
+    loadImageIn();
+}
