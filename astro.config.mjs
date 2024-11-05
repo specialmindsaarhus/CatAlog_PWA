2c4fa6668e132743d59573ac9b211defe5ea6820
specialmindsaarhus <161840253+specialmindsaarhus@users.noreply.github.com>
2024-11-05 07:20:37 +0000

    Changes to making icons work - now with vite-pwa module

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import AstroPWA from '@vite-pwa/astro'
+
 // https://astro.build/config
 export default defineConfig({
     // ... other config options ...
@@ -14,4 +16,38 @@ export default defineConfig({
             },
         },
     },
+    integrations: [
+        AstroPWA({
+          manifest: {
+            name: "My Cat PWA",
+            short_name: "Cat PWA",
+            description: "Til alle katteelskere",
+            start_url: "/",
+            display: "standalone",
+            background_color: "#ffffff",
+            theme_color: "#000000",
+            icons: [
+              {
+                src: "/icon-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+                purpose: "any maskable"
+              },
+              {
+                src: "/icon-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+                purpose: "any maskable"
+              }
+            ]
+          },
+          registerType: 'autoUpdate',
+          workbox: {
+            globPatterns: ['**/*.{js,css,html,png,svg,ico}']
+          },
+          devOptions: {
+            enabled: true
+          }
+        })
+      ]
 });
